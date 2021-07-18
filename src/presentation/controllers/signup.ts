2e260dd63955ee43db78e600456dc58c9b9c15a2
Antônio C. A. L. Filho <antonio.lima16@fatec.sp.gov.br>
2021-07-18 14:30:54 -0300

    feat: ensure signup controller returns an arror if no name is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,8 @@
 export class SignUpController {
   handle (httpRequest: any): any {
     return {
-      status_code: 400
+      status_code: 400,
+      body: new Error('Missing param: name')
     }
   }
 }
