dd38e3b8dff84461cb9389455d62d2acd42268a9
Antônio C. A. L. Filho <antonio.lima16@fatec.sp.gov.br>
2021-07-19 18:57:36 -0300

    refactor: move server error to a helper method

--- src/presentation/helpers/http-helpers.ts
+++ src/presentation/helpers/http-helpers.ts
@@ -1,6 +1,12 @@
+import { ServerError } from '../errors/server-error'
 import { HttpResponse } from '../protocols/http'
 
 export const badRequest = (error: Error): HttpResponse => ({
   status_code: 400,
   body: error
+})
+
+export const serverError = (): HttpResponse => ({
+  status_code: 500,
+  body: new ServerError()
 })
